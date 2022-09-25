30730369dee8dc121f50daff7dec0d2daa0171b2
NaokiTani <naoki.1121.hit.and.run48@gmail.com>
2022-09-25 16:46:53 +0900

    add start clicked

--- app/src/main/java/com/example/blackjack/MainActivity.kt
+++ app/src/main/java/com/example/blackjack/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.blackjack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.text = "clicked"
+
     }
 }
